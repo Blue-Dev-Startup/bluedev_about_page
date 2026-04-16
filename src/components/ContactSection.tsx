565f5a16140fef27d4d551baf788f5c4a731d8f8
Allani Mohamed <mohamedallani909@gmail.com>
2026-04-16 22:37:02 +0100

    updated bluedev contect link

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -5,7 +5,7 @@ const ContactSection = () => {
   const [form, setForm] = useState({ name: "", email: "", message: "" });
 
   const sendEmail = async (emailData) => {
-    const response = await fetch("https://bluedevcontactformscript-production.up.railway.app/api/send", {
+    const response = await fetch("https://bluedev-contact-form-script.onrender.com/send", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(emailData),
